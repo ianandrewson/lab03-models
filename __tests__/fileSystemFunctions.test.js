5696fe12961b6f12af5d906c50344873527e5b8b
Ian Andrewson <ianandrewson@gmail.com>
2019-12-04 14:31:29 -0800

    writeJSON written, all tests pass

--- __tests__/fileSystemFunctions.test.js
+++ __tests__/fileSystemFunctions.test.js
@@ -1,17 +1,32 @@
 const fs = require('fs').promises;
 const fsFuncs = require('../lib/fileSystemFunctions.js');
 
+const mockPath = './my-test-directory/child/more';
+
+const mockDogJson = {
+    name: 'spot',
+    age: 8,
+    weight: '20 lbs'
+};
+
 jest.mock('fs', () => ({
     promises: {
-        mkdir: jest.fn(() => Promise.resolve('./my-test-directory/child/more'))
+        mkdir: jest.fn(() => Promise.resolve(mockPath)),
+        writeFile: jest.fn(() => Promise.resolve(mockPath, mockDogJson)),
     }
 }));
 
 describe('fileSystemFunctions tests', () => {
     it('will make a directory and all parent directories with mkdirp', () => {
-        return fsFuncs.mkdirp('./my-test-directory/child/more')
+        return fsFuncs.mkdirp(mockPath)
             .then(() => {
-                expect(fs.mkdir).toHaveBeenLastCalledWith('./my-test-directory/child/more', 'recursive');
+                expect(fs.mkdir).toHaveBeenLastCalledWith(mockPath, 'recursive');
+            });
+    });
+    it('will write a JSON object to a file', () => {
+        return fsFuncs.writeJSON(mockPath, mockDogJson)
+            .then(() => {
+                expect(fs.writeFile).toHaveBeenLastCalledWith(mockPath, JSON.stringify(mockDogJson));
             });
     });
 });
