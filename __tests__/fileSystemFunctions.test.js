ba6247f4dfbf2d5776aeeec3c8a4ec0eb19e98ac
Ian Andrewson <ianandrewson@gmail.com>
2019-12-04 14:41:55 -0800

    test written for readJSON(), ready to begin TDD

--- __tests__/fileSystemFunctions.test.js
+++ __tests__/fileSystemFunctions.test.js
@@ -3,6 +3,8 @@ const fsFuncs = require('../lib/fileSystemFunctions.js');
 
 const mockPath = './my-test-directory/child/more';
 
+const mockFile = './my-test-directory/child/more/dog';
+
 const mockDogJson = {
     name: 'spot',
     age: 8,
@@ -12,7 +14,9 @@ const mockDogJson = {
 jest.mock('fs', () => ({
     promises: {
         mkdir: jest.fn(() => Promise.resolve(mockPath)),
-        writeFile: jest.fn(() => Promise.resolve(mockPath, mockDogJson)),
+        writeFile: jest.fn(() => Promise.resolve(mockFile, mockDogJson)),
+        readFile: jest.fn(() => Promise.revolve(mockFile)),
+        
     }
 }));
 
@@ -24,9 +28,15 @@ describe('fileSystemFunctions tests', () => {
             });
     });
     it('will write a JSON object to a file', () => {
-        return fsFuncs.writeJSON(mockPath, mockDogJson)
+        return fsFuncs.writeJSON(mockFile, mockDogJson)
             .then(() => {
-                expect(fs.writeFile).toHaveBeenLastCalledWith(mockPath, JSON.stringify(mockDogJson));
+                expect(fs.writeFile).toHaveBeenLastCalledWith(mockFile, JSON.stringify(mockDogJson));
+            });
+    });
+    it('will read a JSON object from a file', () => {
+        return fsFuncs.readJSON(mockFile)
+            .then(() => {
+                expect(fs.readFile).toHaveBeenLastCalledWith(mockFile);
             });
     });
 });
